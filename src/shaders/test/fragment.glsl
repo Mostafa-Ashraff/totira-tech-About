9a919a0909553d69b50cb4c658ea3a4245e78d01
Samar <samar.abdelhamed95@gmail.com>
2023-01-01 07:05:54 +0400

    add img

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -1,4 +1,14 @@
+
+uniform sampler2D uTexture;
+varying vec2 vUv;
+
+
+
+
+
+
+
 void main()
 {
-    gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
+    gl_FragColor = texture2D(uTexture, vUv);
 }
--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -1,4 +1,24 @@
-void main()
-{
+// void main()
+// {
+//     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+// }
+
+
+
+ 
+  varying vec2 vUv;
+  uniform sampler2D utexture1;
+  uniform sampler2D utexture2;
+  varying vec2 offsetText1;
+
+  void main() {
+      offsetText1 = vec2(0.1,0.1);
+      vUv = uv;
+      vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+      vec4 displacement  = texture2D(utexture1,vUv+offsetText1);
+      vec4 displacement2 = texture2D(utexture2,vUv);
+      modelPosition.z   += displacement.r*1.0;
+      modelPosition.z   += displacement2.r*40.0;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-}
+  }
+  
