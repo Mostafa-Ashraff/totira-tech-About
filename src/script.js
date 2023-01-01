11d1b0f12238d8142e35448f67510e1c48f2cb58
Samar <samar.abdelhamed95@gmail.com>
2023-01-01 06:08:05 +0400

    add plane

--- src/script.js
+++ src/script.js
@@ -4,17 +4,25 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
+import vertex from './shaders/test/vertex.glsl'
+import fragment from './shaders/test/fragment.glsl'
 import typefaceFont from 'three/examples/fonts/helvetiker_regular.typeface.json'
 import VirtualScroll from 'virtual-scroll'
+import { Plane } from 'three'
 
-// const scroller = new VirtualScroll()
-// scroller.on(event =>{
-//     wrapper.style.transform ='translateY(f'
-// })
-/**
- * Base
- */
-// Debug
+
+
+var position = 0;
+const scroller = new VirtualScroll();
+scroller.on(event => {
+        // wrapper.style.transform = 'translateY(f'
+        position = event.y / 2000;
+        console.log(position);
+    })
+    /**
+     * Base
+     */
+    // Debug
 const gui = new dat.GUI()
 
 // Canvas
@@ -36,36 +44,35 @@ const fontLoader = new FontLoader()
 
 fontLoader.load(
     '/fonts/helvetiker_regular.typeface.json',
-    (font) =>
-    {
+    (font) => {
         // Material
         const material = new THREE.MeshBasicMaterial({ color: 'white' })
-        // new THREE.ShaderMaterial({
-        //     side: DoubleSide,
-        //     transparent: true,
-        //     defines: {
-        //         IS_SMALL: false,
-        //     },
-        //     extensions: {
-        //         derivatives: true,
-        //     },
-        //     uniforms: {
-        //         // Common
-        //         ...uniforms.common,
-                
+            // new THREE.ShaderMaterial({
+            //     side: DoubleSide,
+            //     transparent: true,
+            //     defines: {
+            //         IS_SMALL: false,
+            //     },
+            //     extensions: {
+            //         derivatives: true,
+            //     },
+            //     uniforms: {
+            //         // Common
+            //         ...uniforms.common,
+
         //         // Rendering
         //         ...uniforms.rendering,
-                
+
         //         // Strokes
         //         ...uniforms.strokes,
         //     },
         //     vertexShader: `
         //         // Attribute
         //         #include <three_msdf_attributes>
-        
+
         //         // Varyings
         //         #include <three_msdf_varyings>
-        
+
         //         void main() {
         //             #include <three_msdf_vertex>
         //         }
@@ -73,24 +80,24 @@ fontLoader.load(
         //     fragmentShader: `
         //         // Varyings
         //         #include <three_msdf_varyings>
-        
+
         //         // Uniforms
         //         #include <three_msdf_common_uniforms>
         //         #include <three_msdf_strokes_uniforms>
-        
+
         //         // Utils
         //         #include <three_msdf_median>
-        
+
         //         void main() {
         //             // Common
         //             #include <three_msdf_common>
-        
+
         //             // Strokes
         //             #include <three_msdf_strokes>
-        
+
         //             // Alpha Test
         //             #include <three_msdf_alpha_test>
-        
+
         //             // Outputs
         //             #include <three_msdf_strokes_output>
         //         }
@@ -99,55 +106,57 @@ fontLoader.load(
         // material.uniforms.uMap.value = atlas;
         // Text
         const texts = ['LOREMMMM',
-        'LOREMMMM',
-        'LOREMMMM',
-        'LOREMMMM',
-        'LOREMMMM',
-        'LOREMMMM',
-        'LOREMMMM',
-        'LOREMMMM',
-        'LOREMMMM',]
-        texts.forEach((txt, i)=>{
-            const textGeometry = new TextGeometry(
-                txt,
-                {   font: font,
-                    size: 0.2,
-                    height: 0.1,
-                    curveSegments: 12,
-                    bevelEnabled: false,
-                    bevelThickness: 0,
-                    bevelSize: 0,
-                    bevelOffset: 0,
-                    bevelSegments: 0}
-            )
-            textGeometry.center()
-            
-            const text = new THREE.Mesh(textGeometry, material)
-            text.position.y = (0.5*i)
-            text.rotation.y = Math.PI *0.12
-            
-            
-            scene.add(text)
-        })
-        // const textGeometry = new TextGeometry(
-        //     'Hello Three.js',
-        //     {
-        //         font: font,
-        //         size: 0.5,
-        //         height: 0.2,
-        //         curveSegments: 12,
-        //         bevelEnabled: true,
-        //         bevelThickness: 0,
-        //         bevelSize: 0.02,
-        //         bevelOffset: 0,
-        //         bevelSegments: 5
-        //     }
-        // )
-        // textGeometry.center()
+            'LOREMMMM',
+            'LOREMMMM',
+            'LOREMMMM',
+            'LOREMMMM',
+            'LOREMMMM',
+            'LOREMMMM',
+            'LOREMMMM',
+            'LOREMMMM',
+        ]
+        texts.forEach((txt, i) => {
+                const textGeometry = new TextGeometry(
+                    txt, {
+                        font: font,
+                        size: 0.2,
+                        height: 0.1,
+                        curveSegments: 12,
+                        bevelEnabled: false,
+                        bevelThickness: 0,
+                        bevelSize: 0,
+                        bevelOffset: 0,
+                        bevelSegments: 0
+                    }
+                )
+                textGeometry.center()
 
-        
+                const text = new THREE.Mesh(textGeometry, material)
+                text.position.y = (0.5 * i)
+                text.rotation.y = Math.PI * 0.12
 
-        
+
+                scene.add(text)
+            })
+            // const textGeometry = new TextGeometry(
+            //     'Hello Three.js',
+            //     {
+            //         font: font,
+            //         size: 0.5,
+            //         height: 0.2,
+            //         curveSegments: 12,
+            //         bevelEnabled: true,
+            //         bevelThickness: 0,
+            //         bevelSize: 0.02,
+            //         bevelOffset: 0,
+            //         bevelSegments: 5
+            //     }
+            // )
+            // textGeometry.center()
+
+
+
+
     }
 )
 
@@ -159,8 +168,7 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -185,8 +193,8 @@ camera.position.z = 2
 scene.add(camera)
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping = true
 
 /**
  * Renderer
@@ -203,12 +211,11 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
     // Update controls
-    controls.update()
+    // controls.update()
 
     // Render
     renderer.render(scene, camera)
@@ -217,4 +224,41 @@ const tick = () =>
     window.requestAnimationFrame(tick)
 }
 
-tick()
+tick();
+var plane;
+// object
+function addObjects() {
+    const materialObj = new THREE.ShaderMaterial({
+        extensions: {
+            derivatives: "#extension GL_OES_standard_derivatives : enable"
+        },
+        side: THREE.DoubleSide,
+        uniforms: {
+            time: { value: 0 },
+            resolution: { value: new THREE.Vector4() },
+        },
+        vertexShader: vertex,
+        fragmentShader: fragment
+    });
+    const geometry = new THREE.PlaneGeometry(1.77, 1, 1, 1).translate(0, 0, 1);
+    let pos = geometry.attributes.position;
+    // let newPos = [];
+    // for (let i = 0; i < pos.length; i += 3) {
+    //     let x = pos[i];
+    //     let y = pos[i + 1];
+    //     let z = pos[i + 3];
+    //     newPos.push(x, y, z);
+    // }
+    // geometry.setAttribute('position', new THREE.Float32BufferAttribute(newPos, 3));
+    plane = new THREE.Mesh(geometry, materialObj);
+    scene.add(plane);
+}
+addObjects();
+
+// render
+function render() {
+    plane.rotation.y = -position;
+    requestAnimationFrame(render);
+    // console.log(plane.rotation.y);
+}
+render();
